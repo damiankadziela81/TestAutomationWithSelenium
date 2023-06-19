ceb04ce296fbdc338846999b0379910cd2e5f248
damiankadziela81 <damiankadziela02@gmail.com>
2023-06-19 13:37:33 +0200

    Remove unecessary code from main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        System.out.println(StringPriceToCents(null));
-
-    }
-
-    private static int StringPriceToCents(String price) {
-        if (price != null) {
-            price =  price.replace("$","").replace(".","");
-            System.out.println(price);
-            if(price.matches("[0-9]+")) {
-                System.out.println("it matches");
-                return Integer.parseInt(price);
-            }
-        }
-        return 0;
     }
 }
