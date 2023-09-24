7ad3ff3cf96b4b5c8a5929bb9940e19754cc3b86
damiankadziela81 <damiankadziela02@gmail.com>
2023-09-24 16:31:22 +0200

    Added 2 new tests for MyStore - sorting on PLP by price

--- src/test/java/org/example/MyStoreTest.java
+++ src/test/java/org/example/MyStoreTest.java
@@ -5,9 +5,28 @@ import org.example.pages.MyStoreMainPage;
 import org.example.pages.MyStorePDP;
 import org.example.pages.MyStorePLP;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MyStoreTest extends BaseTest{
+
+    MyStoreMainPage mainPage;
+    MyStorePLP plp;
+    MyStorePDP pdp;
+    MyStoreCart cart;
+    String baseUrl;
+    @BeforeClass
+    public void before() {
+        mainPage = new MyStoreMainPage(driver);
+        plp = new MyStorePLP(driver);
+        pdp = new MyStorePDP(driver);
+        cart = new MyStoreCart(driver);
+        baseUrl = "http://146.59.32.4/index.php";
+    }
 
     /**
      * check if sum of two products in the cart is calculated correctly
@@ -15,12 +34,8 @@ public class MyStoreTest extends BaseTest{
 
     @Test
     public void productPriceCalculationSummaryInCart() {
-        MyStoreMainPage mainPage = new MyStoreMainPage(driver);
-        MyStorePLP plp = new MyStorePLP(driver);
-        MyStorePDP pdp = new MyStorePDP(driver);
-        MyStoreCart cart = new MyStoreCart(driver);
 
-        driver.get("http://146.59.32.4/index.php");
+        driver.get(baseUrl);
         mainPage.clickOnClothes();
         String page = driver.getCurrentUrl();
         plp.clickProduct1();
@@ -38,5 +53,39 @@ public class MyStoreTest extends BaseTest{
         Assert.assertEquals(product1price + product2price, productsSubtotal);
         Assert.assertEquals(product1price + product2price + shippingCosts, totalPrice);
         System.out.println(product1price + " " + product2price + " " + shippingCosts + " " + totalPrice);
+    }
+
+    /**
+     * check if sorting by price works correctly
+     */
+
+    @Test
+    public void checkSortingByPriceAscending() {
+        driver.get(baseUrl);
+        mainPage.clickOnAccessories();
+        plp.setSortByPriceAscending();
+        List<String> productPrices = plp.readPricesOfAllProductsOnPLP();
+        List<Integer> productPricesInt = productPrices.stream()
+                .map(MyStoreCart::StringPriceToCents)
+                .toList();
+        List<Integer> productPricesIntSorted = new ArrayList<>(productPricesInt);
+        Collections.sort(productPricesIntSorted);
+        Assert.assertEquals(productPricesInt,productPricesIntSorted);
+        System.out.println(productPrices + "\n" + productPricesInt + "\n" + productPricesIntSorted);
+    }
+
+    @Test
+    public void checkSortingByPriceDescending() {
+        driver.get(baseUrl);
+        mainPage.clickOnAccessories();
+        plp.setSortByPriceDescending();
+        List<String> productPrices = plp.readPricesOfAllProductsOnPLP();
+        List<Integer> productPricesInt = productPrices.stream()
+                .map(MyStoreCart::StringPriceToCents)
+                .toList();
+        List<Integer> productPricesIntSorted = new ArrayList<>(productPricesInt);
+        productPricesIntSorted.sort(Collections.reverseOrder());
+        Assert.assertEquals(productPricesInt,productPricesIntSorted);
+        System.out.println(productPrices + "\n" + productPricesInt + "\n" + productPricesIntSorted);
     }
 }
--- src/test/java/org/example/pages/MyStorePLP.java
+++ src/test/java/org/example/pages/MyStorePLP.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.FindBy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyStorePLP extends BasePage{
 
@@ -20,6 +21,17 @@ public class MyStorePLP extends BasePage{
     @FindBy(css = "div:nth-of-type(3) > article h2 > a")
     private WebElement product3;
 
+    @FindBy(css = ".btn-unstyle.select-title")
+    private WebElement sortSelect;
+
+    @FindBy(css = ".dropdown-menu > a:nth-of-type(5)")
+    private WebElement sortByPriceAscending;
+
+    @FindBy(css = ".dropdown-menu > a:nth-of-type(6)")
+    private WebElement sortByPriceDescending;
+
+    @FindBy(css = ".product-price-and-shipping > .price")
+    private List<WebElement> productPrices;
 
     public MyStorePLP(WebDriver driver) {
         super(driver);
@@ -35,6 +47,33 @@ public class MyStorePLP extends BasePage{
 
     public void clickProduct3() {
         product3.click();
+    }
+
+    public void setSortByPriceAscending() {
+        sortSelect.click();
+        sortByPriceAscending.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setSortByPriceDescending() {
+        sortSelect.click();
+        sortByPriceDescending.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> readPricesOfAllProductsOnPLP() {
+        return productPrices
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
     }
 
 
